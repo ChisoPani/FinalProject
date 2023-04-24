baa551c0132f3e5b61bcb6e51ca5aba9758ad9db
StephenAcheampong1 <109006267+StephenAcheampong1@users.noreply.github.com>
2023-04-23 21:46:56 -0400

    Cityupdate

--- FinalProject/Models/FavTeam.cs
+++ FinalProject/Models/FavTeam.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Sport { get; set; }
-        public string Team { get; set; }
+        public string City { get; set; }
         public string TeamColor { get; set; }
 
     }
