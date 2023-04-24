84b924610d8b78bbada627c229d2fb33d28f65f4
Thanay2001 <128756570+Thanay2001@users.noreply.github.com>
2023-04-23 21:35:31 -0400

    Update Hobby.cs

--- FinalProject/Models/Hobby.cs
+++ FinalProject/Models/Hobby.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Weekly_Hours {get; set;}
+        public string Desciption {get; set;}
+        public bool solo_activity {get; set;}
     }
 }
